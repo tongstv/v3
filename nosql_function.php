cf3abb419e654e81ce44911d1e0eded4d3845bbf
Tongstv <tongstv@gmail.com>
2020-03-05 13:27:46 +0700

    fix 05-03

--- nosql_function.php
+++ nosql_function.php
@@ -23,30 +23,33 @@ function loai($num)
 {
 
     $loai = [
-        1 => ['Sim Lục quý', 'sim-luc-quy'],
-        2 => ['Sim ngũ quý', 'sim-ngu-quy'],
+        1 => ['Sim Lục quý', 'sim-luc-quy'], //1
+        2 => ['Sim ngũ quý', 'sim-ngu-quy'], //2
         3 => ['Sim tứ quý', 'sim-tu-quy'], //3
         4 => ['Sim tam hoa kép', 'sim-tam-hoa-kep'], //4
         5 => ['Sim Taxi hai', 'sim-taxi-hai'], //5
         6 => ['Sim Taxi ba', 'sim-taxi-ba'], //6
         7 => ['Sim tam hoa', 'sim-tam-hoa'], //7
-        8 => ['Sim tiến đơn', 'sim-tien-don'], //8
-        9 => ['Sim Lục quý giữa', 'sim-luc-quy-giua'],
-        10 => ['Sim Lộc Phát', 'sim-loc-phat'], //10
-        11 => ['Sim Thần Tài', 'sim-than-tai'], //11
-        12 => ['Sim Ông Địa', 'sim-ong-dia'], //12
-        13 => ['Sim kép', 'sim-kep'], //13
-        14 => ['Sim lặp', 'sim-lap'], //14
-        15 => ['Sim ngũ quý giữa', 'sim-ngu-quy-giua'], //15
-        16 => ['Sim tứ quý giũa', 'sim-tu-quy-giua'], //16
-        17 => ['Sim đảo', 'sim-dao'], //17
-        18 => ['Sim gánh', 'sim-ganh'], //18
-        19 => ['Sim Phú Quý', 'sim-phu-quy'], //19
-        20 => ['Sim đặc biệt', 'sim-dac-biet'], //20
-        21 => ['Sim Năm sinh', 'sim-nam-sinh'], //21
-        22 => ['Sim đầu số cổ', 'sim-dau-co'], //22
-        23 => ['Sim dễ nhớ', 'sim-de-nho'], //23
-        24 => ['sim ngày tháng năm sinh','sim-ngay-thang-nam-sinh']
+        8 => ['Sim tiến kép', 'sim-tien-kep'], //8
+        9 => ['Sim tiến đôi', 'sim-tien-doi'], //9
+        10 => ['Sim gánh kép', 'sim-ganh-kep'], //10
+        11 => ['Sim kép ba', 'sim-kép-ba'], //11
+        12 => ['Sim tiến đơn', 'sim-tien-don'], //12
+        13 => ['Sim Lục quý giữa', 'sim-luc-quy-giua'], //13
+        14 => ['Sim Lộc Phát', 'sim-loc-phat'], //14
+        15 => ['Sim Thần Tài', 'sim-than-tai'], //15
+        16 => ['Sim Ông Địa', 'sim-ong-dia'], //16
+        17 => ['Sim kép', 'sim-kep'], //17
+        18 => ['Sim lặp', 'sim-lap'], //18
+        19 => ['Sim ngũ quý giữa', 'sim-ngu-quy-giua'], //19
+        20 => ['Sim tứ quý giũa', 'sim-tu-quy-giua'], //20
+        21 => ['Sim đảo', 'sim-dao'], //21
+        22 => ['Sim gánh', 'sim-ganh'], //22
+        23 => ['Sim Phú Quý', 'sim-phu-quy'], //23
+        24 => ['Sim đặc biệt', 'sim-dac-biet'], //24
+        25 => ['Sim Năm sinh', 'sim-nam-sinh'], //25
+        26 => ['Sim đầu số cổ', 'sim-dau-co'], //26
+        27 => ['Sim dễ nhớ', 'sim-de-nho'], //27
     ];
     return $loai[$num] ? $loai[$num] : '';
 }
@@ -62,28 +65,30 @@ function loais()
         'sim-taxi-hai' => 5, /// nâng cấp
         'sim-taxi-ba' => 6, // bổ xung
         'sim-tam-hoa' => 7,
-        'sim-tien-don' => 8,
-        'sim-luc-quy-giua' => 9,
-        'sim-loc-phat' => 10,
-        'sim-than-tai' => 11,
-        'sim-ong-dia' => 12,
-        'sim-kep' => 13, // bổ xung
-        'sim-lap' => 14, // nâng cấp
-        'sim-ngu-quy-giua' => 15,
-        'sim-tu-quy-giua' => 16,
-        'sim-dao' => 17, // bổ xung
-        'sim-ganh' => 18, /// sim gánh
-        'sim-phu-quy' => 19,
-        'sim-dac-biet' => 20,
-        'sim-nam-sinh' => 21,
-        'sim-dau-co' => 22,
-        'sim-de-nho' => 23,
-        'sim-ngay-thang-nam-sinh' => 21
+        'sim-tien-kep' => 8, // bổ xung
+        'sim-tien-doi' => 9, // bổ xung
+        'sim-ganh-kep' => 10, // bổ xung
+        'sim-kep-ba' => 11, // bổ xung
+        'sim-tien-don' => 12,
+        'sim-luc-quy-giua' => 13,
+        'sim-loc-phat' => 14,
+        'sim-than-tai' => 15,
+        'sim-ong-dia' => 16,
+        'sim-kep' => 17, // bổ xung
+        'sim-lap' => 18, // nâng cấp
+        'sim-ngu-quy-giua' => 19,
+        'sim-tu-quy-giua' => 20,
+        'sim-dao' => 21, // bổ xung
+        'sim-ganh' => 22, /// sim gánh
+        'sim-phu-quy' => 23,
+        'sim-dac-biet' => 24,
+        'sim-nam-sinh' => 25,
+        'sim-dau-co' => 26,
+        'sim-de-nho' => 27,
 
     );
     return $loais;
 }
-
 function phanloai($sim1)
 {
     $sim2 = preg_replace('/[^0-9]/', '', $sim1);
@@ -113,40 +118,114 @@ function phanloai($sim1)
         return 6; // sim taxi ba
     } else if (in_array(substr($sim2, -3), array_merge(['000'], range(111, 999, 111)))) {
         return 7; // sim tam hoa
-    } else if ((in_array(substr($sim2, -3, 3), ['012', '123', '234', '345', '456', '567', '678', '789'])) || (in_array(substr($sim2, -4, 4), ['0123', '1234', '2345', '3456', '4567', '5678', '6789', '1357', '0246'])) || (in_array(substr($sim2, -5, 5), ['01234', '12345', '23456', '34567', '45678', '56789'])) || (in_array(substr($sim2, -6, 6), ['012345', '123456', '234567', '345678', '456789']))) {
-        return 8; // tim tiến lên
-    } else if (strstr_array_needle($sim2, array_merge(['000000'], range(111111, 999999, 111111)))) {
-        return 9; // sim lục quý giữa
-    } else if (in_array(substr($sim2, -2, 2), ['68', '86'])) {
-        return 10; // lộc phát
-    } else if (in_array(substr($sim2, -2, 2), ['39', '79'])) {
-        return 11; // sim thần tài
-    } else if (in_array(substr($sim2, -2, 2), ['38', '78'])) {
-        return 12; // sim ông địa
-    } else if (substr($sim2, -4, 1) == substr($sim2, -3, 1) && substr($sim2, -2, 1) == substr($sim2, -1, 1) && (substr($sim2, -3, 1) != substr($sim2, -2, 1))) {
-        return 13;  //  sim kép
-    } else if (substr($sim2, -4, 1) == substr($sim2, -2, 1) && substr($sim2, -3, 1) == substr($sim2, -1, 1) && substr($sim2, -2, 1) != substr($sim2, -1, 1)) {
-        return 14;  // sim lặp
-    } else if (strstr_array_needle($sim2, array_merge(['00000'], range(11111, 99999, 11111)))) {
-        return 15;  //sim ngũ quý giũa
-    } else if (strstr_array_needle($sim2, array_merge(['0000'], range(1111, 9999, 1111))) and substr($sim2, -2, 1) != substr($sim2, -1, 1)) {
-        return 16;  // sim tứ quý giữa
-    } else if (((substr($sim2, -4, 1) == substr($sim2, -1, 1)) && (substr($sim2, -3, 1) == substr($sim2, -2, 1)) && (substr($sim2, -2, 1) != substr($sim2, -1, 1))) || ((substr($sim2, -5, 1) == substr($sim2, -1, 1)) && (substr($sim2, -2, 1) != substr($sim2, -1, 1)) && ((substr($sim2, -4, 1) == substr($sim2, -3, 1)) && (substr($sim2, -3, 1) == substr($sim2, -2, 1)))) || ((substr($sim2, -6, 1) == substr($sim2, -1, 1)) && (substr($sim2, -4, 1) == substr($sim2, -3, 1)) && (substr($sim2, -5, 1) == substr($sim2, -2, 1)))) {
-        return 17; // sim đảo
-    } else if ((($r61 == $r41) && ($r61 == $r31) && ($r61 == $r11) && ($r61 != $r51) && ($r61 != $r21) && ($r51 != $r21)) || (($r61 == $r41) && ($r51 == $r21) && ($r31 == $r11) && ($r61 != $r51) && ($r61 != $r31) && ($r51 != $r31)) || (($r61 == $r41) && ($r31 == $r11) && ($r61 != $r51) && ($r61 != $r31) && ($r61 != $r21) && ($r51 != $r31) && ($r51 != $r21) && ($r31 != $r21)) || (($r61 == $r21) && ($r51 == $r11) && ($r41 == $r31) && ($r61 != $r51) && ($r61 != $r41) && ($r51 != $r41)) || (($r61 == $r51) && ($r61 == $r21) && ($r61 == $r11) && ($r61 != $r41) && ($r61 != $r31) && ($r41 != $r31))) {
-        return 18; // sim ganh
-    } else if (in_array(substr($sim2, -2, 2), ['66', '88', '99'])) {
-        return 19; // sim phú quý
-    } else if (in_array(substr($sim2, -4, 4), ['1102', '2204', '1618', '4404', '4953', '4078', '3456', '8683', '0378', '1668', '8386', '8683']) || substr($sim2, -7, 7) == '6677028' || substr($sim2, -5) == '36578' || substr($sim2, -6, 6) == '154078' || substr($sim2, -6, 6) == '838689' || substr($sim2, -5) == '55078') {
-        return 20; // sim số độc
-    } else if (in_array(substr($sim2, -4, 4), range(1950, date('Y'))) || (in_array(substr($sim2, -6, 2), range(1, 31)) && in_array(substr($sim2, -4, 2), range(1, 12))) && (in_array(substr($sim2, -2), range(50, 99)) || in_array(substr($sim2, -2), range(00, 21)))) {
-        return 21; // sim năm sinh
-    } else if (in_array(substr($sim2, 0, 4), ['0903', '0908', '0909', '0913', '0918', '0919', '0983', '0986', '0988', '0989', '0977', '0979', '0929', '0926', '0993', '0996', '0879'])) {
-        return 22; // đầu số cổ
+    } else if (
 
-    } else {
-        return 23;
-    }
+        (in_array($r62, array_merge(['00'], range(11, 99, 11)))
+            && in_array($r42, array_merge(['00'], range(11, 99, 11)))
+            && in_array($r22, array_merge(['00'], range(11, 99, 11)))
+            && (($r41 == $r61 + 1 && $r21 == $r61 + 2)
+                || ($r41 == $r61 + 2 && $r21 == $r61 + 4)
+                || ($r41 == $r61 + 3 && $r21 == $r61 + 6)))
+
+        ||
+        (
+            in_array($r62, array_merge(['00'], range(11, 99, 11)))
+            && in_array($r42, array_merge(['00'], range(11, 99, 11)))
+            && in_array($r22, array_merge(['00'], range(11, 99, 11)))
+            && ($r31 > $r51  && $r11 > $r31)
+
+        )
+
+
+    ) {
+        return 8; // sim-tien-kep
+
+
+    } else if (
+        (($r61 == $r41 && $r41 == $r21)
+
+            && (
+                ($r11 == $r31 + 1) && ($r11 == $r51 + 2)
+                || (($r11 == $r31 + 2) && ($r11 == $r51 + 4))
+                || (($r11 == $r31 + 3) && ($r11 == $r51 + 6))
+
+
+            )
+
+        )
+        ||
+        (
+            ($r51 == $r31 && $r31 == $r11)
+
+            && (
+                ($r21 == $r41 + 1 && $r21 == $r61 + 2)
+                || ($r21 == $r41 + 2 && $r21 == $r61 + 4)
+                || ($r21 == $r41 + 3 && $r21 == $r61 + 6)
+            )
+
+        )
+
+
+
+    ) {
+        return 9; //sim-tien-doi
+
+    } else if (
+
+        in_array($r62, array_merge(['00'], range(11, 99, 11)))
+        && in_array($r42, array_merge(['00'], range(11, 99, 11)))
+        && in_array($r22, array_merge(['00'], range(11, 99, 11)))
+
+        && ($r31 != $r11)
+        && ($r62 == $r22)
+
+    ) {
+        return 10; //sim-ganh-kep
+
+    } else
+        if (in_array($r62, array_merge(['00'], range(11, 99, 11)))
+            && in_array($r42, array_merge(['00'], range(11, 99, 11)))
+            && in_array($r22, array_merge(['00'], range(11, 99, 11)))
+
+            && ($r51 != $r31 && $r31 != $r11)
+
+        ) {
+            return 11; // sim-kep-ba
+
+        } else if ((in_array(substr($sim2, -3, 3), ['012', '123', '234', '345', '456', '567', '678', '789'])) || (in_array(substr($sim2, -4, 4), ['0123', '1234', '2345', '3456', '4567', '5678', '6789', '1357', '0246'])) || (in_array(substr($sim2, -5, 5), ['01234', '12345', '23456', '34567', '45678', '56789'])) || (in_array(substr($sim2, -6, 6), ['012345', '123456', '234567', '345678', '456789']))) {
+            return 12; // tim tiến đơn
+        } else if (strstr_array_needle($sim2, array_merge(['000000'], range(111111, 999999, 111111)))) {
+            return 13; // sim lục quý giữa
+        } else if (in_array(substr($sim2, -2, 2), ['68', '86'])) {
+            return 14; // lộc phát
+        } else if (in_array(substr($sim2, -2, 2), ['39', '79'])) {
+            return 15; // sim thần tài
+        } else if (in_array(substr($sim2, -2, 2), ['38', '78'])) {
+            return 16; // sim ông địa
+        } else if (substr($sim2, -4, 1) == substr($sim2, -3, 1) && substr($sim2, -2, 1) == substr($sim2, -1, 1) && (substr($sim2, -3, 1) != substr($sim2, -2, 1))) {
+            return 17;  //  sim kép
+        } else if (substr($sim2, -4, 1) == substr($sim2, -2, 1) && substr($sim2, -3, 1) == substr($sim2, -1, 1) && substr($sim2, -2, 1) != substr($sim2, -1, 1)) {
+            return 18;  // sim lặp
+        } else if (strstr_array_needle($sim2, array_merge(['00000'], range(11111, 99999, 11111)))) {
+            return 19;  //sim ngũ quý giũa
+        } else if (strstr_array_needle($sim2, array_merge(['0000'], range(1111, 9999, 1111))) and substr($sim2, -2, 1) != substr($sim2, -1, 1)) {
+            return 20;  // sim tứ quý giữa
+        } else if (((substr($sim2, -4, 1) == substr($sim2, -1, 1)) && (substr($sim2, -3, 1) == substr($sim2, -2, 1)) && (substr($sim2, -2, 1) != substr($sim2, -1, 1))) || ((substr($sim2, -5, 1) == substr($sim2, -1, 1)) && (substr($sim2, -2, 1) != substr($sim2, -1, 1)) && ((substr($sim2, -4, 1) == substr($sim2, -3, 1)) && (substr($sim2, -3, 1) == substr($sim2, -2, 1)))) || ((substr($sim2, -6, 1) == substr($sim2, -1, 1)) && (substr($sim2, -4, 1) == substr($sim2, -3, 1)) && (substr($sim2, -5, 1) == substr($sim2, -2, 1)))) {
+            return 21; // sim đảo
+        } else if ((($r61 == $r41) && ($r61 == $r31) && ($r61 == $r11) && ($r61 != $r51) && ($r61 != $r21) && ($r51 != $r21)) || (($r61 == $r41) && ($r51 == $r21) && ($r31 == $r11) && ($r61 != $r51) && ($r61 != $r31) && ($r51 != $r31)) || (($r61 == $r41) && ($r31 == $r11) && ($r61 != $r51) && ($r61 != $r31) && ($r61 != $r21) && ($r51 != $r31) && ($r51 != $r21) && ($r31 != $r21)) || (($r61 == $r21) && ($r51 == $r11) && ($r41 == $r31) && ($r61 != $r51) && ($r61 != $r41) && ($r51 != $r41)) || (($r61 == $r51) && ($r61 == $r21) && ($r61 == $r11) && ($r61 != $r41) && ($r61 != $r31) && ($r41 != $r31))) {
+            return 22; // sim ganh
+        } else if (in_array(substr($sim2, -2, 2), ['66', '88', '99'])) {
+            return 23; // sim phú quý
+        } else if (in_array(substr($sim2, -4, 4), ['1102', '2204', '1618', '4404', '4953', '4078', '3456', '8683', '0378', '1668', '8386', '8683']) || substr($sim2, -7, 7) == '6677028' || substr($sim2, -5) == '36578' || substr($sim2, -6, 6) == '154078' || substr($sim2, -6, 6) == '838689' || substr($sim2, -5) == '55078') {
+            return 24; // sim đặc biệt
+        } else if (in_array(substr($sim2, -4, 4), range(1950, date('Y'))) || (in_array(substr($sim2, -6, 2), range(1, 31)) && in_array(substr($sim2, -4, 2), range(1, 12))) && (in_array(substr($sim2, -2), range(50, 99)) || in_array(substr($sim2, -2), range(00, 21)))) {
+            return 25; // sim năm sinh
+        } else if (in_array(substr($sim2, 0, 4), ['0903', '0908', '0909', '0913', '0918', '0919', '0983', '0986', '0988', '0989', '0977', '0979', '0929', '0926', '0993', '0996', '0879'])) {
+            return 26; // đầu số cổ
+
+        } else {
+            return 27;
+        }
 }
 
 
